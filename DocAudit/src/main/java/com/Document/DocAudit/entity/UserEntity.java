447310825fb36aba8f1eb72dbfe4050f9624cac8
suryaa-developer <suryaarul2001@gmail.com>
2026-02-17 23:21:13 +0530

    refactor : added Entity

--- DocAudit/src/main/java/com/Document/DocAudit/entity/UserEntity.java
+++ DocAudit/src/main/java/com/Document/DocAudit/entity/UserEntity.java
@@ -1,20 +1,34 @@
 package com.Document.DocAudit.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Table(name = "users",uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"provider","providerId"}
+        )})
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId; // user id
+    @Column(nullable = false)
     private String provider; // user login provider name(eg. google)
+    @Column(nullable = false)
     private String providerId;// user login provider id
+    @Column(nullable = false)
     private String email; // user Email
     private String name; // user name
     private String pictureUrl; // user profile url
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime lastLoginAt;
+
 }
