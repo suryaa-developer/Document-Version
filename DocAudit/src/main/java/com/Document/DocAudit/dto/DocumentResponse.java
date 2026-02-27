37abd37c4e1b16225c19ea8bbbfc8ef184313ce4
suryaa-developer <suryaarul2001@gmail.com>
2026-02-27 23:16:15 +0530

    feat : added doc reponse

---
+++ DocAudit/src/main/java/com/Document/DocAudit/dto/DocumentResponse.java
@@ -0,0 +1,18 @@
+package com.Document.DocAudit.dto;
+
+import com.Document.DocAudit.enums.DocumentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentResponse {
+    private Long Id;
+    private String Title;
+    private String Content;
+    private String CreatedBy;
+    private String CreatedByUserId;
+    private DocumentStatus Status;
+}
