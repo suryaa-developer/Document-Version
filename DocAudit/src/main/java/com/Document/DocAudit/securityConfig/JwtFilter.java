b2e6e93d8b4702b78cdc6634cd723d4bd9fe3212
suryaa-developer <suryaarul2001@gmail.com>
2026-03-04 23:44:15 +0530

    feat : created authenticated token and validation

---
+++ DocAudit/src/main/java/com/Document/DocAudit/securityConfig/JwtFilter.java
@@ -0,0 +1,52 @@
+    package com.Document.DocAudit.securityConfig;
+
+    import jakarta.servlet.FilterChain;
+    import jakarta.servlet.ServletException;
+    import jakarta.servlet.http.HttpServletRequest;
+    import jakarta.servlet.http.HttpServletResponse;
+    import org.jspecify.annotations.NonNull;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+    import org.springframework.security.core.context.SecurityContextHolder;
+    import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+    import org.springframework.stereotype.Component;
+    import org.springframework.web.filter.OncePerRequestFilter;
+
+    import java.io.IOException;
+    import java.util.ArrayList;
+
+    @Component
+    public class JwtFilter extends OncePerRequestFilter {
+        @Autowired
+        private JwtUtils jwtUtils;
+
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+            String authHeader = request.getHeader("Authorization");
+
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = authHeader.substring(7);
+                if (jwtUtils.ValidateToken(token)) {
+                    String username = jwtUtils.GetUsernameFromToken(token);
+
+                    // 1. Create the Authentication Token
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            username, null, new ArrayList<>()
+                    );
+
+                    // 2. Set details (important for Spring to trust the request)
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    // 3. Set the context manually
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+
+                    System.out.println("User Authenticated in SecurityContext: " + username);
+                } else {
+                    System.err.println("JWT Token was invalid or expired");
+                }
+            }
+
+            // Always call this so the request moves to the Controller
+            filterChain.doFilter(request, response);
+        }
+    }
