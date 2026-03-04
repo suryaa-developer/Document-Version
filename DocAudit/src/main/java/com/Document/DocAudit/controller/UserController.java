0a31e88700a8d1cf3be02e01b3afb8fea627057d
suryaa-developer <suryaarul2001@gmail.com>
2026-03-04 23:34:04 +0530

    feat : added default success url api method

--- DocAudit/src/main/java/com/Document/DocAudit/controller/UserController.java
+++ DocAudit/src/main/java/com/Document/DocAudit/controller/UserController.java
@@ -3,26 +3,45 @@ package com.Document.DocAudit.controller;
 import com.Document.DocAudit.dto.UserResponse;
 import com.Document.DocAudit.entity.UserEntity;
 import com.Document.DocAudit.securityConfig.CustomUserPrincipal;
+import com.Document.DocAudit.securityConfig.JwtUtils;
+import com.Document.DocAudit.securityConfig.SecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("login")
+@RequestMapping("/login")
 public class UserController {
 
-    @GetMapping("/user")
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @GetMapping("/usercreate")
     public UserResponse GetUser(@AuthenticationPrincipal CustomUserPrincipal principal){
         UserEntity user = principal.getUser();
+        // 1. Generate the token specifically for this logged-in user
+        String jwtToken = jwtUtils.generateToken(user.getName());
         return new UserResponse(
                 user.getUserId(),
                 user.getEmail(),
                 user.getName(),
                 user.getPictureUrl(),
                 user.getProvider(),
-                user.getStatus()
-
+                user.getStatus(),
+                jwtToken
         );
     }
+    @GetMapping("/success")
+    public String LoginSuccess(){
+        String Username = SecurityConfig.GetCurrentUser();
+        return Username +" Login Success";
+    }
+
+    @GetMapping("/api/secure-data")
+    public String secureData() {
+        return "Sensitive info";
+    }
+
 }
