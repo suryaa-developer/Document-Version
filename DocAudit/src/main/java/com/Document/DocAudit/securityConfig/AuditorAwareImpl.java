183908c68717064d7451675a04c52bbf0e617318
suryaa-developer <suryaarul2001@gmail.com>
2026-03-12 22:56:35 +0530

    feat : added implemnted class to validate user

---
+++ DocAudit/src/main/java/com/Document/DocAudit/securityConfig/AuditorAwareImpl.java
@@ -0,0 +1,21 @@
+package com.Document.DocAudit.securityConfig;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorProvider")
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication auth =SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated()) {
+            return Optional.empty();
+        }
+        return Optional.of(auth.getName());
+    }
+}
